aaba79650ddacd95159f90f6c5534d06fbf325f9
mimanshi-tiwari <tiwari.mimanshi@gmail.com>
2026-09-02 21:32:41 -0700

    patch: dev community setup, server

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express(); // instance of new express js application
+
+app.use("/test", (req, res) => {
+    res.send("Hello from express js");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000!");
+});
